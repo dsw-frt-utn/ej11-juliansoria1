0086979ee965a1b4cb162cce94c4e4ef4a38334d
Julian Soria <julianoscarsoria@gmail.com>
2026-05-22 00:15:18 -0300

    casoList Terminado

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,5 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
-
+﻿using System;
+using System.Collections.Generic;
+namespace Dsw2026Ej11.Collections;
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
 //Incluir un método para retornar la lista
@@ -8,4 +9,12 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+   
+    List<string> alumnos = new List<string>();
+
+    foreach(string alumno in new string[5]{"Pedro" , "Juan", "Jaime", "Ana", "Luis"})
+    {
+        alumnos.Add(alumno);
+    }
+
 }
